8c6360db8dd54e8201e62bd2c58270cbb38e2f16
lyonsyonii <liam@garriga.dev>
2025-03-06 16:21:49 +0100

    add lab3

---
+++ Lab3/src/main.cpp
@@ -0,0 +1,233 @@
+#include <Arduino.h>
+#include "Arduino_FreeRTOS.h"
+#include "timers.h"
+
+//circular buffer for debugging
+#define BUFF_SIZE 250
+float t[BUFF_SIZE] = {};
+byte circ_buffer1[BUFF_SIZE] = {};
+byte circ_buffer2[BUFF_SIZE] = {};
+byte circ_buffer3[BUFF_SIZE] = {};
+byte circ_buffer4[BUFF_SIZE] = {};
+byte circ_buffer5[BUFF_SIZE] = {};
+byte circ_buffer6[BUFF_SIZE] = {};
+float debug_data1[BUFF_SIZE] = {};
+unsigned int circ_buffer_counter = 0;
+
+//Task handlers
+TaskHandle_t Task1Handle;
+TaskHandle_t Task2Handle;
+TaskHandle_t Task3Handle;
+TaskHandle_t Task4Handle;
+TaskHandle_t Task5Handle;
+TaskHandle_t Task6Handle;
+
+//Timer handlers
+TimerHandle_t xOneShotTimer;
+BaseType_t xOneShotStarted;
+
+//WakeUpTime
+TickType_t xLastWakeTime1;
+TickType_t xLastWakeTime2;
+TickType_t xLastWakeTime3;
+TickType_t xLastWakeTime4;
+TickType_t xLastWakeTime5;
+TickType_t xLastWakeTime6;
+
+//Function prototypes
+void Task1(void *pvParameters);
+void Task2(void *pvParameters);
+void Task3(void *pvParameters);
+void Task4(void *pvParameters);
+void Task5(void *pvParameters);
+void Task6(void *pvParameters);
+void OneShotTimerCallback( TimerHandle_t xTimer );
+void str_trace(void);
+void str_compute(unsigned int);
+float str_getTime(void);
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  xOneShotTimer = xTimerCreate("OneShotTimer",   pdMS_TO_TICKS( 10000 ) , pdFALSE, 0, OneShotTimerCallback );
+  xOneShotStarted = xTimerStart( xOneShotTimer, 0 );
+
+  xTaskCreate(Task1,"Task1",configMINIMAL_STACK_SIZE ,NULL,8,&Task1Handle);
+  xTaskCreate(Task2,"Task2",configMINIMAL_STACK_SIZE ,NULL,6,&Task2Handle);
+  xTaskCreate(Task3,"Task3",configMINIMAL_STACK_SIZE ,NULL,7,&Task3Handle);
+  xTaskCreate(Task4,"Task4",configMINIMAL_STACK_SIZE ,NULL,5,&Task4Handle);
+  xTaskCreate(Task5,"Task5",configMINIMAL_STACK_SIZE ,NULL,4,&Task5Handle);
+  xTaskCreate(Task6,"Task6",configMINIMAL_STACK_SIZE ,NULL,3,&Task6Handle);
+
+  // Initialise the xLastWakeTime variable with the current time.
+  xLastWakeTime1 = 0;
+  xLastWakeTime2 = xLastWakeTime1;
+  xLastWakeTime3 = xLastWakeTime1;
+  xLastWakeTime4 = xLastWakeTime1;
+  xLastWakeTime5 = xLastWakeTime1;
+  xLastWakeTime6 = xLastWakeTime1;
+
+  //vTaskStartScheduler(); //Most ports require calling this to start the kernel
+}
+
+void loop() 
+{
+
+}
+
+void Task1(void *pvParameters)
+{
+  pinMode(LED_BUILTIN,OUTPUT);
+  for(;;)
+  {
+    digitalWrite(LED_BUILTIN,digitalRead(LED_BUILTIN)^1);
+    str_compute(10);
+    vTaskDelayUntil( &xLastWakeTime1, pdMS_TO_TICKS(50) );
+  }
+}
+
+void Task2(void *pvParameters)
+{
+  for(;;)
+  {
+    str_compute(15);
+    vTaskDelayUntil( &xLastWakeTime2, pdMS_TO_TICKS(100) );
+  }
+}
+
+void Task3(void *pvParameters)
+{
+  for(;;)
+  {
+    str_compute(4);
+    vTaskDelayUntil( &xLastWakeTime3, pdMS_TO_TICKS(50) );
+  }
+}
+
+void Task4(void *pvParameters)
+{
+  for(;;)
+  {
+    str_compute(6);
+    vTaskDelayUntil( &xLastWakeTime4, pdMS_TO_TICKS(150) );
+  }
+}
+
+void Task5(void *pvParameters)
+{
+  for(;;)
+  {
+    str_compute(8);
+    vTaskDelayUntil( &xLastWakeTime5, pdMS_TO_TICKS(100) );
+  }
+}
+
+void Task6(void *pvParameters)
+{
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A3,INPUT);
+  pinMode(A4,INPUT);
+  pinMode(A5,INPUT);
+
+  for(;;)
+  {
+    int adcA1=analogRead(A1);
+    int adcA2=analogRead(A2);
+    int adcA3=analogRead(A3);
+    int adcA4=analogRead(A4);
+    int adcA5=analogRead(A5);
+    Serial.println("OSC");
+		Serial.print(adcA1);
+		Serial.print(",");
+		Serial.print(adcA2);
+		Serial.print(",");
+		Serial.print(adcA3);
+		Serial.print(",");
+		Serial.print(adcA4);
+		Serial.print(",");
+		Serial.print(adcA5);
+		Serial.println(); 
+    str_compute(54);
+    vTaskDelayUntil( &xLastWakeTime6, pdMS_TO_TICKS(200) );
+  }
+}
+
+void OneShotTimerCallback( TimerHandle_t xTimer )
+{
+  TickType_t xTimeNow;
+  xTimeNow = xTaskGetTickCount();
+  //oneshottimer_count++;
+  //stop the kernel...
+  // vTaskSuspend(Task1Handle);
+  // vTaskSuspend(Task2Handle);
+  // vTaskSuspend(Task3Handle);
+  // vTaskSuspend(Task4Handle);
+  // vTaskSuspend(Task5Handle);
+  // vTaskSuspend(Task6Handle);
+  vTaskSuspendAll();
+
+  	//...and sent data to the host PC
+	unsigned int i;
+	for (i = 0; i < BUFF_SIZE; i++)
+	{
+		Serial.println("DAT");
+		Serial.print((float)t[i]);
+		Serial.print(",");
+		Serial.write((uint8_t)circ_buffer1[i]);
+		Serial.print(",");
+		Serial.write((uint8_t)circ_buffer2[i]);
+		Serial.print(",");
+		Serial.write((uint8_t)circ_buffer3[i]);
+		Serial.print(",");
+		Serial.write((uint8_t)circ_buffer4[i]);
+		Serial.print(",");
+		Serial.write((uint8_t)circ_buffer5[i]);
+		Serial.print(",");
+		Serial.write((uint8_t)circ_buffer6[i]);
+		Serial.print(",");        
+		Serial.print((float)debug_data1[i]);
+		Serial.println();
+	}
+}
+
+
+void str_trace(void)
+{
+  circ_buffer_counter++;
+  if (circ_buffer_counter >= BUFF_SIZE)
+  {
+    circ_buffer_counter = 0;
+  }
+
+  t[circ_buffer_counter] = str_getTime();//sent time in milliseconds
+  circ_buffer1[circ_buffer_counter] = eTaskGetState(Task1Handle);
+  circ_buffer2[circ_buffer_counter] = eTaskGetState(Task2Handle);
+  circ_buffer3[circ_buffer_counter] = eTaskGetState(Task3Handle);
+  circ_buffer4[circ_buffer_counter] = eTaskGetState(Task4Handle);
+  circ_buffer5[circ_buffer_counter] = eTaskGetState(Task5Handle);
+  circ_buffer6[circ_buffer_counter] = eTaskGetState(Task6Handle);
+  debug_data1[circ_buffer_counter]=2.7;
+}
+
+//str_compute(x) is only used to waste time without using delays
+void str_compute(unsigned int milliseconds)
+{
+  unsigned int i = 0;
+  unsigned int imax = 0;
+  imax = milliseconds * 92;
+  volatile float dummy = 1;
+  for (i = 0; i < imax; i++)
+  {
+    dummy = dummy * dummy;
+  }
+}
+
+float str_getTime(void)
+{
+  //float t=(float)(0.5e-3*((float)OCR1A*xTaskGetTickCount()+TCNT1));//Sent time in milliseconds!!!
+  float t = 10.0 * (float)(xTaskGetTickCount()) + 0.0005 * (float)TCNT1; 
+  return t;
+}
+
