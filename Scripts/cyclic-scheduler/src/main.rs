82a541b1a21930aca41db34e82f3f543a362953c
lyonsyonii <liam@garriga.dev>
2025-04-06 08:54:31 +0200

    greatly enhance results

--- Scripts/cyclic-scheduler/src/main.rs
+++ Scripts/cyclic-scheduler/src/main.rs
@@ -4,41 +4,9 @@ fn main() {
     let input = std::env::args().nth(1).unwrap_or("input.txt".to_owned());
     let input = std::fs::read_to_string(input).unwrap();
 
-    let mut smallest = 1.;
-    let raw_tasks = input.lines().map(|line| {
-        let mut split = line.split(',');
-        let name = split.next().expect("Expected task name");
-        let computing_time = split.next().and_then(|s| s.parse::<f64>().ok()).expect("Expected computing time");
-        let period = split.next().and_then(|s| s.parse::<f64>().ok()).expect("Expected period");
-
-        let fract = computing_time.fract();
-        if fract > 0. && fract < smallest {
-            smallest = fract;
-        }
-        let fract = period.fract();
-        if fract > 0. && fract < smallest {
-            smallest = fract;
-        }
-        (name, computing_time, period)
-    }).collect::<Vec<_>>();
-
-    let base= if smallest < 1. {
-        10u64.pow(smallest.log10().abs() as u32)
-    } else {
-        1
-    };
-
-    // Normalize numbers to integers
-    let tasks: &mut [Task] = &mut raw_tasks.into_iter().map(|(name, computing_time, period)| {
-        Task {
-            name: name.to_owned(),
-            computing_time: (computing_time * base as f64) as u64,
-            period: (period * base as f64) as u64,
-        }
-    }).collect::<Vec<_>>();
+    let tasks = &mut parse_tasks(input);
 
     dbg!(&tasks);
-    dbg!(base);
     let utilization = dbg!(utilization(tasks));
     if utilization > 1. {
         println!("utilization greater than 1, schedule does not exist!");
@@ -50,13 +18,76 @@ fn main() {
         println!("no secondary periods found, schedule does not exist!");
         std::process::exit(1);
     }
-    for frame_time in frame_times {
-        dbg!(frame_time, schedule(tasks, frame_time, hyperperiod));
+
+    let possible_schedules = frame_times
+        .into_iter()
+        .map(|frame_time| schedule(tasks, frame_time, hyperperiod).map(|s| (s, frame_time)));
+    for (schedule, frame_time) in possible_schedules.flatten() {
+        println!("Schedule(frame_time = {frame_time}):");
+        for (frame, frame_tasks) in schedule.iter().enumerate() {
+            println!("  [Frame {frame}]");
+            let mut start = frame_time * frame as u64;
+            for (Task { name, computing_time, period }, activation) in frame_tasks {
+                let deadline = activation + period;
+                println!("  {name}: {{\n    activation: {activation},\n    deadline: {deadline},\n    start: {start}\n  }}");
+                start += computing_time;
+            }
+            println!();
+        }
     }
 }
 
+fn parse_tasks(input: String) -> Vec<Task> {
+    let mut smallest = 1.;
+    let raw_tasks = input
+        .lines()
+        .map(|line| {
+            let mut split = line.split(',');
+            let name = split.next().expect("Expected task name");
+            let computing_time = split
+                .next()
+                .and_then(|s| s.parse::<f64>().ok())
+                .expect("Expected computing time");
+            let period = split
+                .next()
+                .and_then(|s| s.parse::<f64>().ok())
+                .expect("Expected period");
+
+            let fract = computing_time.fract();
+            if fract > 0. && fract < smallest {
+                smallest = fract;
+            }
+            let fract = period.fract();
+            if fract > 0. && fract < smallest {
+                smallest = fract;
+            }
+            (name, computing_time, period)
+        })
+        .collect::<Vec<_>>();
+
+    let base = if smallest < 1. {
+        10u64.pow(smallest.log10().abs() as u32)
+    } else {
+        1
+    };
+    dbg!(base);
+
+    // Normalize numbers to integers
+    raw_tasks
+        .into_iter()
+        .map(|(name, computing_time, period)| Task {
+            name: name.to_owned(),
+            computing_time: (computing_time * base as f64) as u64,
+            period: (period * base as f64) as u64,
+        })
+        .collect::<Vec<_>>()
+}
+
 fn utilization(tasks: &[Task]) -> f64 {
-    tasks.iter().map(|t| t.computing_time as f64 / t.period as f64).sum()
+    tasks
+        .iter()
+        .map(|t| t.computing_time as f64 / t.period as f64)
+        .sum()
 }
 
 fn hyperperiod(tasks: &[Task]) -> u64 {
@@ -75,10 +106,10 @@ fn frame_times(tasks: &[Task], hyperperiod: u64) -> Vec<u64> {
                     println!("Found secondary period for k = {k}; period = {period}");
                     periods.push(period);
                     break;
-                },
+                }
                 std::cmp::Ordering::Greater => {
                     break;
-                },
+                }
             }
         }
     }
@@ -100,15 +131,18 @@ fn frame_times(tasks: &[Task], hyperperiod: u64) -> Vec<u64> {
     periods
 }
 
-fn schedule(tasks: &mut [Task], frame_time: u64, hyperperiod: u64) -> Option<Vec<Vec<(String, u64)>>> {
+fn schedule(
+    tasks: &mut [Task],
+    frame_time: u64,
+    hyperperiod: u64,
+) -> Option<Vec<Vec<(Task, u64)>>> {
     tasks.sort_unstable_by(|a, b| a.period.cmp(&b.period));
-    
+
     let num_frames = hyperperiod / frame_time;
     let mut available_time = vec![frame_time; num_frames as usize + 1];
-    let mut schedule = vec![Vec::<(String, u64)>::new(); num_frames as usize + 1];
+    let mut schedule = vec![Vec::<(Task, u64)>::new(); num_frames as usize + 1];
     for task in tasks {
         let num_jobs = hyperperiod / task.period;
-        dbg!(&task.name, num_jobs);
         for i in 0..num_jobs {
             let frame = (task.period * i).div_ceil(frame_time);
             let mut scheduled = false;
@@ -116,7 +150,7 @@ fn schedule(tasks: &mut [Task], frame_time: u64, hyperperiod: u64) -> Option<Vec
             while frame + c_off <= num_frames && c_off * frame_time <= task.period {
                 let c_fr = (frame + c_off) as usize;
                 if available_time[c_fr] >= task.computing_time {
-                    schedule[c_fr].push((task.name.clone(), i * task.period));
+                    schedule[c_fr].push((task.clone(), i * task.period));
                     available_time[c_fr] -= task.computing_time;
                     scheduled = true;
                     break;
@@ -125,7 +159,7 @@ fn schedule(tasks: &mut [Task], frame_time: u64, hyperperiod: u64) -> Option<Vec
             }
             if !scheduled {
                 println!("Can't schedule period #{i} of task {}", task.name);
-                // return None;
+                return None;
             }
         }
     }
