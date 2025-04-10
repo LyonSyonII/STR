fc61518e865e5667184aa4c1b993e43846140ca2
lyonsyonii <liam@garriga.dev>
2025-04-10 18:50:02 +0200

    add tasks

--- Lab4-Segway/src/main.cpp
+++ Lab4-Segway/src/main.cpp
@@ -11,142 +11,177 @@
 // char password[] = "wlan_str";        // your network password (use for WPA, or use as key for WEP)
 // IPAddress ipLocal=IPAddress(192, 168, 1, 102);//fixed ip not working...!!!
 
+struct Offsets {
+    float xAccOffset;
+    float yAccOffset;
+    float zAccOffset;
+    float xGyroOffset;
+    float yGyroOffset;
+    float zGyroOffset;
+};
+
+TaskHandle_t task1MoveMotorHandle;
+TaskHandle_t task9DebugHandle;
+
 const float rZero = -99.75;  // reference pitch angle
 float r = rZero;             // variable to deal with varying reference
 float error;
 float error_k_1 = 0;
-float kp = 2.5;  // the magic gains
-float ki = 75.00;
-float kd = 25.0;
-float ku = 0.6;
-float P = 0;  // the controller
-float I = 0;
-float D = 0;
-float u = 0;
 float gX = 0;  // gyro data
 float gY = 0;
 float gZ = 0;
 float aX = 0;  // acceleration data
 float aY = 0;
 float aZ = 0;
-float xAcc = 0;
-float yAcc = 0;
-float zAcc = 0;
-float xGyro = 0;
-float yGyro = 0;
-float zGyro = 0;
-float xAccOffset = 0;
-float yAccOffset = 0;
-float zAccOffset = 0;
-float xGyroOffset = 0;
-float yGyroOffset = 0;
-float zGyroOffset = 0;
-float omega = 0;
-float xOmega = 0;
-float pitch = 0;
-float roll = 0;
-float pitch_filtered = 0;
-float roll_filtered = 0;
-float pit = 0;
-float rol = 0;
-float yaw = 0;
-long int tStart = 0;
-long int tElapsed = 0;
-float h = 0.010;          // sampling time in sec
-long int delayTime = 10;  // delay time in millisec
 float vBatt = 0;
+float pitch_filtered = 0;
 
 // Function prototypes
 void initWifi(void);
-void initM5StickCPlus(void);
-void IMUCalibration(void);
+Offsets initM5StickCPlus(void);
+Offsets IMUCalibration(void);
+
+void task1MoveMotor(const Offsets& offsets);
+void task9Debug(void*);
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Init...");
     
-    initM5StickCPlus();
+    Offsets offsets = initM5StickCPlus();
+
+    xTaskCreate(
+        (void(*)(void*))task1MoveMotor,
+        "task1MoveMotor",
+        configMINIMAL_STACK_SIZE*3,
+        &offsets,
+        9,
+        &task1MoveMotorHandle
+    );
+
+    xTaskCreate(
+        task9Debug,
+        "task9Debug",
+        configMINIMAL_STACK_SIZE*3,
+        NULL,
+        1,
+        &task9DebugHandle
+    );
 }
 
 void loop() {
-    tStart = millis();
+    // tStart = millis();
 
-    // Task: ReadSensors
-    M5.Imu.getGyroData(&gX, &gY, &gZ);   // gyroscope
-    M5.Imu.getAccelData(&aX, &aY, &aZ);  // accelerometer
+    // tElapsed = millis() - tStart;
+    // delayTime = (long int)(h * 1000.0f) - tElapsed;
+    // if (delayTime < 0) delayTime = 0;
+    // if (delayTime > 10) delayTime = 10;
+    // delay(delayTime);  // Periodic call
+}
 
-    xAcc = aX;
-    yAcc = aY;
-    zAcc = aZ;
-    xGyro = gX - xGyroOffset;
-    yGyro = gY - yGyroOffset;
-    zGyro = gZ - zGyroOffset;
-    // -xGyro*M5.IMU.gRes 
-    // M5.IMU.gRes=0.01 https://docs.m5stack.switch-science.com/en/arduino/m5stickc/sh200q_m5stickc degrees per
-    // second. TODO: check it
-    xOmega = -xGyro * 0.01;
+void task1MoveMotor(const Offsets &offsets) {    
+    // setup
+    TickType_t lastWake = 0;
 
-    roll = -360.0 / 6.28 * atan2(-xAcc, zAcc);
-    pitch = 360.0 / 6.28 * atan2(yAcc, zAcc);
-    // low pass filter + complementary filter (handmade). TODO: double check it!
-    pitch_filtered = 0.993 * (pitch_filtered + xGyro * h) + (1.0 - 0.993) * (pitch);
+    // const float h = 0.010; // 10 ms
+    const float h = 0.005; // 10 ms
+    float kp = 2.5;  // the magic gains
+    float ki = 75.00;
+    float kd = 25.0;
+    float ku = 0.6;
+    
+    float P = 0;  // the controller
+    float I = 0;
+    float D = 0;
+    float u = 0;
 
-    r = rZero;
-    error = r - pitch_filtered;
-    P = kp * error;
-    I = I + ki * h * error;
-    if (I > 100) I = 100;
-    if (I < -100) I = -100;
-    D = kd * xOmega;
-    u = P + I + D + ku * u;
+    // loop
+    while (1) {
+        // long start = millis();
 
-    // if the error is big, do nothing since the segway has fallen
-    if (abs(error) > 30) {
-        u = 0;
+        // Task: ReadSensors
+        M5.Imu.getGyroData(&gX, &gY, &gZ);   // gyroscope
+        M5.Imu.getAccelData(&aX, &aY, &aZ);  // accelerometer
+    
+        auto xAcc = aX;
+        auto yAcc = aY;
+        auto zAcc = aZ;
+        auto xGyro = gX - offsets.xGyroOffset;
+        auto yGyro = gY - offsets.yGyroOffset;
+        auto zGyro = gZ - offsets.zGyroOffset;
+        // -xGyro*M5.IMU.gRes 
+        // M5.IMU.gRes=0.01 https://docs.m5stack.switch-science.com/en/arduino/m5stickc/sh200q_m5stickc degrees per
+        // second. TODO: check it
+        auto xOmega = -xGyro * 0.01;
+    
+        auto roll = -360.0 / 6.28 * atan2(-xAcc, zAcc);
+        auto pitch = 360.0 / 6.28 * atan2(yAcc, zAcc);
+        // low pass filter + complementary filter (handmade). TODO: double check it!
+        pitch_filtered = 0.993 * (pitch_filtered + xGyro * h) + (1.0 - 0.993) * (pitch);
+    
+        r = rZero;
+        error = r - pitch_filtered;
+        P = kp * error;
+        I = I + ki * h * error;
+        if (I > 100) I = 100;
+        if (I < -100) I = -100;
+        D = kd * xOmega;
+        u = P + I + D + ku * u;
+    
+        // if the error is big, do nothing since the segway has fallen
+        if (abs(error) > 70) {
+            u = 0;
+        }
+        // check for saturation
+        if (u > 127) u = 127;  
+        if (u < -127) u = -127;
+    
+        // send data to motors
+        Wire.beginTransmission(0x38);
+        // channel left  
+        Wire.write(0);                 
+        Wire.write((int)(-u));
+        Wire.endTransmission();
+        Wire.beginTransmission(0x38);
+        // channel right
+        Wire.write(1);  
+        Wire.write((int)(u));
+        Wire.endTransmission();
+        
+        // long end = millis();
+        // Serial.printf("task1 took %lu ms\n", end - start);
+        
+        vTaskDelayUntil(&lastWake, pdMS_TO_TICKS(h * 1000));
     }
-    // check for saturation
-    if (u > 127) u = 127;  
-    if (u < -127) u = -127;
+}
 
-    // send data to motors
-    Wire.beginTransmission(0x38);
-    // channel left  
-    Wire.write(0);                 
-    Wire.write((int)(-u));
-    Wire.endTransmission();
-    Wire.beginTransmission(0x38);
-    // channel right
-    Wire.write(1);  
-    Wire.write((int)(u));
-    Wire.endTransmission();
+void task9Debug(void*) {
+    TickType_t lastWake = 0;
+    while (1) {        
+        vBatt = M5.Axp.GetBatVoltage();
+        M5.Lcd.setCursor(0, 10);
+        M5.Lcd.printf("v=%5.2fV   ", vBatt);
+        M5.Lcd.setCursor(0, 40);
+        M5.Lcd.printf("r=%5.2f   ", r);
+        M5.Lcd.setCursor(0, 70);
+        M5.Lcd.printf("pitch=%d   ", (int)pitch_filtered);
 
-    vBatt = M5.Axp.GetBatVoltage();
-    M5.Lcd.setCursor(0, 10);
-    M5.Lcd.printf("v=%5.2fV   ", vBatt);
-    M5.Lcd.setCursor(0, 40);
-    M5.Lcd.printf("r=%5.2f   ", r);
-    M5.Lcd.setCursor(0, 70);
-    M5.Lcd.printf("pitch=%d   ", (int)pitch_filtered);
+        Serial.println("OSC");
+        Serial.print((float)vBatt);
+        Serial.print(",");
+        Serial.print((float)r);
+        Serial.print(",");
+        Serial.print((float)pitch_filtered);
+        Serial.println();
 
-    Serial.println("OSC");
-    Serial.print((float)vBatt);
-    Serial.print(",");
-    Serial.print((float)r);
-    Serial.print(",");
-    Serial.print((float)pitch_filtered);
-    Serial.print(",");
-    Serial.print(delayTime);
-    Serial.println();
-
-    tElapsed = millis() - tStart;
-    delayTime = (long int)(h * 1000.0f) - tElapsed;
-    if (delayTime < 0) delayTime = 0;
-    if (delayTime > 10) delayTime = 10;
-    delay(delayTime);  // Periodic call
+        xTaskDelayUntil(&lastWake, pdMS_TO_TICKS(200));
+    }
 }
 
 // IMUCalibration is only used to remove gyroscope offset
-void IMUCalibration(void) {
+Offsets IMUCalibration(void) {
+    Offsets offsets;
+
     unsigned int calibrationIter = 10000;
     M5.Lcd.fillScreen(BLACK);
     Serial.println("Calibrating IMU... do not move the IMU");
@@ -155,25 +190,27 @@ void IMUCalibration(void) {
         M5.Imu.getAccelData(&aX, &aY, &aZ);
         M5.Imu.getGyroData(&gX, &gY, &gZ);
         // M5.Imu.getAhrsData(&pit, &rol, &yaw);
-        xAccOffset += aX;
-        yAccOffset += aY;
-        zAccOffset += aZ;
-        xGyroOffset += gX;
-        yGyroOffset += gY;
-        zGyroOffset += gZ;
+        offsets.xAccOffset += aX;
+        offsets.yAccOffset += aY;
+        offsets.zAccOffset += aZ;
+        offsets.xGyroOffset += gX;
+        offsets.yGyroOffset += gY;
+        offsets.zGyroOffset += gZ;
     }
-    xAccOffset = xAccOffset / (float)calibrationIter;
-    yAccOffset = yAccOffset / (float)calibrationIter;
-    zAccOffset = zAccOffset / (float)calibrationIter - 1.0;
-    xGyroOffset = xGyroOffset / (float)calibrationIter;
-    yGyroOffset = yGyroOffset / (float)calibrationIter;
-    zGyroOffset = zGyroOffset / (float)calibrationIter;
+    offsets.xAccOffset = offsets.xAccOffset / (float)calibrationIter;
+    offsets.yAccOffset = offsets.yAccOffset / (float)calibrationIter;
+    offsets.zAccOffset = offsets.zAccOffset / (float)calibrationIter - 1.0;
+    offsets.xGyroOffset = offsets.xGyroOffset / (float)calibrationIter;
+    offsets.yGyroOffset = offsets.yGyroOffset / (float)calibrationIter;
+    offsets.zGyroOffset = offsets.zGyroOffset / (float)calibrationIter;
     Serial.println("IMU calibrated!");
     M5.Lcd.println("Done");
     M5.Lcd.fillScreen(BLACK);
+
+    return offsets;
 }
 
-void initM5StickCPlus(void) {
+Offsets initM5StickCPlus(void) {
     pinMode(M5_LED, OUTPUT);
     pinMode(M5_BUTTON_HOME, INPUT);
     pinMode(M5_BUTTON_RST, INPUT);
@@ -206,8 +243,9 @@ void initM5StickCPlus(void) {
 
     // initWifi();
 
-    IMUCalibration();
+    Offsets ret = IMUCalibration();
     M5.Lcd.fillScreen(BLACK);
+    return ret;
 }
 
 void initWifi(void) {
