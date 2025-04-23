39512dc808543ecac3ce03ac04c0c09d8992584b
Ziang Dai <zaingdai@gmail.com>
2025-04-23 17:36:08 +0200

    Updated python script

--- Lab4-Segway/main.py
+++ Lab4-Segway/main.py
@@ -3,14 +3,14 @@ import socket
 
 UDP_IP="172.20.10.100"
 UDP_PORT=8888
-MESSAGE=b"f"
+MESSAGE="f"
 
 def main():
     """
     Main method
     """
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    sock.sendto(bytes(MESSAGE, encoding="utf-8"), (UDP_IP, UDP_PORT))
 
 if __name__ == "__main__":
     main()
